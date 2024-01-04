546f2714c232ae77e95c9bcbf272e343b0ea80e6
Yuuko Martins <yuukomartins@pm.me>
2024-01-03 22:51:12 -0300

    move project to Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/martins-yuu/now_playing
+
+go 1.21.5
--- test/now_playing_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:now_playing/now_playing.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
